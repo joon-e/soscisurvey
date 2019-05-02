6a48a1b4b028f617e280e40652f842dd753e4833
Julian Unkel <julian.unkel@gmail.com>
2019-05-02 16:56:03 +0200

    Initial commit

---
+++ R/helpers.R
@@ -0,0 +1,42 @@
+#' Make label vector
+#'
+#' Creates a named vector of value labels for all variables
+#'
+#' @import dplyr
+make_label_vector <- function(variable, values) {
+  value.labels <- filter(values, var == variable)
+  value.labels <- select(value.labels, value, label)
+  value.labels <- setNames(value.labels$value, value.labels$label)
+  return(value.labels)
+}
+
+#' Add value labels
+#'
+#' Adds value labels to a variable
+add_label <- function(variable, values) {
+  if (length(values) > 0) {
+    labelled::val_labels(variable) <- values
+  }
+  return(variable)
+}
+
+#' Make missings vector
+#'
+#' Creates a vector missing values for all variables
+#'
+#' @import dplyr
+make_missing_vector <- function(variable, missings) {
+  miss.vals <- missings %>%
+    filter(var == variable) %>%
+    pull(missing)
+}
+
+#' Add value labels
+#'
+#' Adds missings to variable
+add_missings <- function(variable, missings) {
+  if (length(missings) > 0) {
+    labelled::na_values(variable) <- missings
+  }
+  return(variable)
+}
---
+++ R/sosci.R
@@ -0,0 +1,83 @@
+#' Load data from SoSciSurvey
+#'
+#' Loads data from SoSciSurvey via the API and creates a labelled tibble.
+#'
+#' @import dplyr
+#' @import purrr
+#' @import tibble
+#' @import tidyr
+#' @import labelled
+#'
+#' @param URL SoSciSurvey API URL
+#' @param ... Additional parameters for API retrieval. See:
+#'     https://www.soscisurvey.de/help/doku.php/en:results:data-api
+#'
+#' @return A labelled tibble
+#'
+#' @export
+sosci <- function(URL, ...) {
+
+  params <- list(...)
+
+  # Add variable info to API parameters
+  params[["infoVariables"]] <- TRUE
+  params[["infoValues"]] <- TRUE
+
+  # Get data
+  resp <- httr::GET(URL, query = params)
+  httr::stop_for_status(resp)
+  json <- httr::content(resp, "parsed")
+
+  # Get dataset
+  data <- json$data %>%
+    bind_rows()
+
+  # Get variable info
+  varinfo <- json$variables
+
+  vars <- tibble(
+    var = names(varinfo),
+    label = unlist(map(varinfo, "label")),
+    type = unlist(map(varinfo, "type"))
+  ) %>%
+    filter(var %in% names(data))
+
+  # Get value labels
+  val.labels <- varinfo %>%
+    enframe("var", "varinfo") %>%
+    mutate(label = map(varinfo, "values", .null = list())) %>%
+    select(var, label) %>%
+    filter(label %>% map(length) > 0) %>%
+    mutate(value = map(label, names)) %>%
+    filter(value %>% map(length) > 0) %>%
+    unnest(value, label) %>%
+    unnest() %>%
+    mutate(value = as.integer(value))
+
+  # Get missings
+  missings <- varinfo %>%
+    enframe("var", "varinfo") %>%
+    mutate(missing = map(varinfo, "missing", .null = list())) %>%
+    select(var, missing) %>%
+    filter(missing %>% map(length) > 0) %>%
+    unnest() %>%
+    unnest()
+
+  # Add variable labels
+  var.labels <- setNames(as.character(vars$label), vars$var)
+  var_label(data) <- var.labels
+
+  # Add type attribute
+  var.types <- tolower(pull(vars, type))
+  data <- modify2(data, var.types, `attr<-`, which = "var.type")
+
+  # Add value labels
+  val.labs <- map(names(data), make_label_vector, val.labels)
+  data <- modify2(data, val.labs, add_label)
+
+  # Add missings
+  miss.vals <- map(names(data), make_missing_vector, missings)
+  data <- modify2(data, miss.vals, add_missings)
+
+  return(data)
+}
