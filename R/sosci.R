3810ad1ad9d026ee3ebf8d389cb8811729d5008f
Julian Unkel <julian.unkel@gmail.com>
2019-05-03 11:29:02 +0200

    Added variable info attributes

--- R/sosci.R
+++ R/sosci.R
@@ -22,6 +22,7 @@ sosci <- function(URL, ...) {
   # Add variable info to API parameters
   params[["infoVariables"]] <- TRUE
   params[["infoValues"]] <- TRUE
+  params[["infoQuestionText"]] <- TRUE
 
   # Get data
   resp <- httr::GET(URL, query = params)
@@ -37,8 +38,11 @@ sosci <- function(URL, ...) {
 
   vars <- tibble(
     var = names(varinfo),
-    label = unlist(map(varinfo, "label")),
-    type = unlist(map(varinfo, "type"))
+    label = unlist(map(varinfo, "label", .null = NA)),
+    type = unlist(map(varinfo, "type", .null = NA)),
+    input = unlist(map(varinfo, "input", .null = NA)),
+    question = unlist(map(varinfo, "question", .null = NA)),
+    sysvar = unlist(map(varinfo, "sysvar", .null = NA))
   ) %>%
     filter(var %in% names(data))
 
@@ -67,9 +71,15 @@ sosci <- function(URL, ...) {
   var.labels <- setNames(as.character(vars$label), vars$var)
   var_label(data) <- var.labels
 
-  # Add type attribute
+  # Add attribute
   var.types <- tolower(pull(vars, type))
-  data <- modify2(data, var.types, `attr<-`, which = "var.type")
+  var.input <- tolower(pull(vars, input))
+  var.question <- pull(vars, question)
+  var.sysvar <- pull(vars, sysvar)
+  data <- modify2(data, var.types, `attr<-`, which = "var.type") %>%
+    modify2(var.input, `attr<-`, which = "var.input") %>%
+    modify2(var.question, `attr<-`, which = "var.question") %>%
+    modify2(var.sysvar, `attr<-`, which = "var.sysvar")
 
   # Add value labels
   val.labs <- map(names(data), make_label_vector, val.labels)
